5b1d30e71703c3891f6beb9a2b9c8118ee6307f0
shkinderI <shkinder.ilia@gmail.com>
2023-07-22 21:58:21 +0300

    Initial Commit

---
+++ Svetofor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Svetofor
+//
+//  Created by Ilia on 22.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
